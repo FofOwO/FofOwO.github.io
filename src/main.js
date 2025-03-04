a627c3112732eba2034316b9ea1a9bb56ae59f44
FofOwO <105993972+FofOwO@users.noreply.github.com>
2025-03-04 09:31:05 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -18,12 +18,13 @@ L.tileLayer('https://tile.jawg.io/9e2efa26-cd51-4996-9b56-6f76849fa505/{z}/{x}/{
 
 // Données des playlists par continent
 var playlists = { //Dictionnaires de playlists
-  "Europe": "https://open.spotify.com/embed/playlist/4MCSvXgcWBu3ShaCtTbB89?utm_source=generator&theme=0",
-  "Amerique du Nord": "https://open.spotify.com/embed/playlist/4MCSvXgcWBu3ShaCtTbB89?utm_source=generator&theme=0",
-  "Afrique": "https://open.spotify.com/embed/playlist/4MCSvXgcWBu3ShaCtTbB89?utm_source=generator&theme=0",
-  "Asie": "https://open.spotify.com/embed/playlist/4MCSvXgcWBu3ShaCtTbB89?utm_source=generator&theme=0",
-  "Oceanie": "https://open.spotify.com/embed/playlist/4MCSvXgcWBu3ShaCtTbB89?utm_source=generator&theme=0",
-  "Amerique du Sud": "https://open.spotify.com/embed/playlist/0deJgJeYkA36dDBcCeV67z?utm_source=generator&theme=0"
+  "Europe": "https://open.spotify.com/embed/playlist/4phLOaUqkSHvA0DGNEqFhd?si=a44814c8244042ec&pt=c89130790b771bac3c2aff5b0b21219f&theme=0",
+  "Amerique du Nord": "https://open.spotify.com/embed/playlist/2i4c496r5pqDcBcrPgZi39?si=273582bf216a47ed&pt=4153185ea7be14fb4254fe08cb1a7247&theme=0",
+  "Afrique": "https://open.spotify.com/embed/playlist/3SDwVe49p0psP5fOlsgSqF?si=2bc7acec7c124897&pt=a732fcfb084d8022de88f3e6b466282a&theme=0",
+  "Asie": "https://open.spotify.com/embed/playlist/262ujecvemI6NbGo2AAj9P?si=4d5535a1a5d44ee4&pt=f22ed12060ced8a0074144a4211d7d12&theme=0",
+  "Oceanie": "https://open.spotify.com/embed/playlist/0qiazrwvI8z3Vj9lECeAXO?si=9786ba9e38714a25&pt=cdeb961a55b4b7a20f5fc9984cec8349&theme=0",
+  "Amerique du Sud": "https://open.spotify.com/embed/playlist/39ppLyYXShbUptXaPJfGvB?si=673e89b87d074336&pt=96e9c8ef3f07af7ee56e5e43f2a46389&theme=0",
+  "Antarctique": "https://open.spotify.com/embed/playlist/1P7WRBN9zVDx153XYcEuTi?si=4dd69773c8eb403e&pt=d17d02240aac4b8034705d750307c15d&theme=0"
 
 };
 
@@ -34,7 +35,8 @@ var locations = { //dictionnaires des locations
   "Afrique": [0, 20],
   "Asie": [30, 100],
   "Oceanie": [-25, 135],
-  "Amerique du Sud": [-15, -65]
+  "Amerique du Sud": [-15, -65],
+  "Antarctique": [-80, 0]
 };
 
 // Ajouter les marqueurs
